7076661df52175abd470b9228309bd6cfbb849f6
lvainio <leojvainio@gmail.com>
2023-01-09 15:54:07 +0100

    hej

---
+++ src/Direction.java
@@ -0,0 +1,13 @@
+/**
+ * This enum class holds all the possible directions
+ * a player can move in. 
+ * 
+ * @author Leo Vainio, lvainio@kth.se
+ */
+
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
